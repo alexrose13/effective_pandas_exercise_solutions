46b98a2d7a05a5deac6c66c9b4880b53481df9f9
Alexander Rose <alexander.h.rose@gmail.com>
2022-04-06 09:58:20 -0600

    publishing updated logo and minor text corrections

--- _build/jupyter_execute/02_Installation.py
+++ _build/jupyter_execute/02_Installation.py
@@ -5,7 +5,15 @@
 
 # ## Exercises 1 & 2
 
-# Additional installation instructions can be found as part of the Anaconda Documentation [here](https://docs.anaconda.com/anaconda/navigator/install/)
+# > Install pandas on your machine (using Anaconda or pip)
+# 
+# > Install Jupyter on your machine
+
+# Installation instructions for `pandas` can be found [here](https://pandas.pydata.org/docs/getting_started/install.html)
+# 
+# Installation instructions for `Jupyter` can be found [here](https://jupyter.org/install)
+# 
+# Additional installation instructions can be found as part of the Anaconda Documentation [here](https://docs.anaconda.com/anaconda/navigator/install/). Anaconda comes out of the box with both `pandas` and `Jupyter`. 
 
 # ## Exercise 3
 
